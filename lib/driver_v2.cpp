9ce2b45c95eab3379cab157e46b71fdbb812a36c
Kendall Garcia <garciaks@yahoo.com>
2026-04-05 04:16:09 -0700

    cleanup

--- lib/driver_v2.cpp
+++ lib/driver_v2.cpp
@@ -2,6 +2,13 @@
 #include <SPI.h>
 #include <math.h>
 
+/*
+ * DWM3000 SPI Driver Architecture
+ * 1. CS pin toggling is strictly handled inside the lowest-level SPI transfer function for atomicity.
+ * 2. Data buffers are strictly uint8_t arrays.
+ * 3. Dynamic header sizing is handled internally by readWriteRegister; user code only passes reg/sub_reg.
+ */
+
 DWM3000Class DWM3000;
 
 // Internal variables for the driver
@@ -19,18 +26,13 @@ DWM3000_Config_t DWM3000Class::config = {
     PHR_RATE_850KB     // phr_rate
 };
 
-void DWM3000Class::begin() 
+void DWM3000Class::begin() //Made by @kendagaz
 {
 
     delay(5);
     pinMode(CS_PIN, OUTPUT);
 
-
-    SPI.beginTransaction(SPISettings(2000000, MSBFIRST, SPI_MODE0)); // 2 MHz clock speed, max speed of DW3000 is 38MHz, but ESP32
-                                                                     // base clock is 80MHz; SPI hardware must be 80/(integer)
-                                                                     // MODE0 is sample HIGH on rising edge
-                                                                     // MSBFIRST defined by DW3000 User Manual
-    SPI.endTransaction();
+    SPI.begin();
     digitalWrite(CS_PIN, HIGH);
 
     if (DEBUG_OUTPUT == 1) {
@@ -39,45 +41,33 @@ void DWM3000Class::begin()
 }
 
 
-void DWM3000Class::spiTransaction(uint8_t txBuffer[] , size_t txLength, size_t rxBuffer[], uint32_t rxLength) {
+void DWM3000Class::spiTransaction(uint8_t txBuffer[] , size_t txLength, size_t rxBuffer[], uint32_t rxLength) { //Made by @kendagaz
     // Passing txBuffer[] is a pointer to &txBuffer[0]
 
 
-
-
-
-
-
-
+    SPI.beginTransaction(SPISettings(20000000, MSBFIRST, SPI_MODE0));
 
     digitalWrite(CS_PIN, LOW); // Pull CS low to start transaction
-
+    
     // Put txBuffer data on the SPI bus
     // MOSI wire
     for (size_t i = 0; i < txLength; i++) { 
         SPI.transfer(txBuffer[i]);
     }
 
+    SPI.endTransaction(); 
+    digitalWrite(CS_PIN, HIGH); // Pull CS high to end transaction
+}
+
+void DWM3000Class::readWriteRegister() { //Made by @kendagaz
+    // Passing txBuffer[] is a pointer to &txBuffer[0]
 
 
+    SPI.beginTransaction(SPISettings(20000000, MSBFIRST, SPI_MODE0));
+
+    digitalWrite(CS_PIN, LOW); // Pull CS low to start transaction
 
 
-   
-
-
-
-
-
-    
-    
-
-
-
-
-
-
-
-
-
-
+    SPI.endTransaction(); 
+    digitalWrite(CS_PIN, HIGH); // Pull CS high to end transaction
 }
